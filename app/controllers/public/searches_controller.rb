0a3ee7baa4f56619fbc4db2730735409be84dada
EC2 Default User <ec2-user@ip-172-31-47-7.ap-northeast-1.compute.internal>
2023-04-20 12:30:55 +0000

    [add]admin/user

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,18 +1,9 @@
 class Public::SearchesController < ApplicationController
 
   def search
-    @range = params[:range]
-
-    if @range == "User"
-      @users = User.looks(params[:search], params[:word])
-    else
-      @books = Book.looks(params[:search], params[:word])
-    end
-  end
-
-  def search
     if params[:word].present?
-      @reviews = Review.where('caption LIKE ?', "%#{params[:word]}%")
+      @word = params[:word]
+      @reviews = Review.where('cafe_name LIKE(?) OR address LIKE(?) OR opening_hours LIKE(?) OR review LIKE(?)', "%#{params[:word]}%", "%#{params[:word]}%", "%#{params[:word]}%", "%#{params[:word]}%")
       @searchword = params[:word]
     else
       @reviews = Review.all
