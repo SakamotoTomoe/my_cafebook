21f16f8289f4532443bdc68c2ac1abff3edfda6a
EC2 Default User <ec2-user@ip-172-31-47-7.ap-northeast-1.compute.internal>
2023-05-17 12:58:59 +0000

    [レイアウト]admin/review

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -20,13 +20,13 @@ class Admin::SessionsController < Devise::SessionsController
 
   protected
   
-  def after_sign_in_path_for(resource)
-    admin_reviews
-  end
+  #def after_sign_in_path_for(resource)
+    #admin_reviews
+  #end
 
-  def after_sign_out_path_for(resource)
-    new_admin_session_path
-  end
+  #def after_sign_out_path_for(resource)
+    #new_admin_session_path
+  #end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
