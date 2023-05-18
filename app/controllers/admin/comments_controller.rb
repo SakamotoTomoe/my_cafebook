f72d36b601cd4aebf0887e7801efa0b0b847765d
EC2 Default User <ec2-user@ip-172-31-47-7.ap-northeast-1.compute.internal>
2023-05-18 09:32:07 +0000

    [レイアウト]admin

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -14,6 +14,12 @@ class Admin::CommentsController < ApplicationController
     redirect_to admin_comments_path
   end
 
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to admin_comments_path
+  end
+
   private
 
   def comments_params
