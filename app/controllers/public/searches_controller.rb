8754b529aa7c4cc1c8428823df932f325e1f5d14
EC2 Default User <ec2-user@ip-172-31-47-7.ap-northeast-1.compute.internal>
2023-05-20 13:46:54 +0000

    [レイアウト]public/search

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,22 +1,23 @@
 class Public::SearchesController < ApplicationController
 
   def search
-    if params[:word].present?
-      @word = params[:word]
-      @reviews = Review.where('cafe_name LIKE(?) OR address LIKE(?) OR opening_hours LIKE(?) OR review LIKE(?)', "%#{params[:word]}%", "%#{params[:word]}%", "%#{params[:word]}%", "%#{params[:word]}%")
-      @searchword = params[:word]
-    else
-      @reviews = Review.all
-    end
+    @keywords = Keyword.all
   end
 
   def search_results
-    @keywords = Keyword.find(params[:keywords])
-    @reviews = []
-    @keywords.each do |keyword|
-      @reviews = @reviews + keyword.reviews
+    if params[:word].present?
+      @word = params[:word]
+      @reviews = Review.where('cafe_name LIKE(?) OR address LIKE(?) OR opening_hours LIKE(?) OR review LIKE(?)', "%#{params[:word]}%", "%#{params[:word]}%", "%#{params[:word]}%", "%#{params[:word]}%")
+    elsif params[:keywords].present?
+      @keywords = Keyword.find(params[:keywords])
+      @reviews = []
+      @keywords.each do |keyword|
+        @reviews = @reviews + keyword.reviews
+      end
+      @reviews.uniq
+    else
+      @reviews = Review.all
     end
-    @reviews.uniq
   end
 
 end
