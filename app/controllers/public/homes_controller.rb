524adfca169ad3d8c1f671f33a20b190958aba63
EC2 Default User <ec2-user@ip-172-31-47-7.ap-northeast-1.compute.internal>
2023-05-22 12:24:35 +0000

    テスト実施2

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,8 +1,8 @@
 class Public::HomesController < ApplicationController
 
   def top
-    @reviews = Review.page(params[:page])
-    @comments = Comment.all
+    @reviews = Review.page(params[:page]).order(created_at: :desc)
+    @comments = Comment.page(params[:comment_page]).per(3).order(created_at: :desc)
   end
 
   def index
