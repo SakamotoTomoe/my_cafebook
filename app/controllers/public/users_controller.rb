99bb83d4a1695c00fffd99e479e2837442fed953
EC2 Default User <ec2-user@ip-172-31-47-7.ap-northeast-1.compute.internal>
2023-07-02 06:43:23 +0000

    [delete]html

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -31,7 +31,7 @@ class Public::UsersController < ApplicationController
     params.require(:user).permit(:user_name, :email, :introduction, :image)
   end
 
-   def is_matching_login_user
+  def is_matching_login_user
     user = User.find(params[:id])
     unless user.id == current_user.id
       redirect_to root_path
